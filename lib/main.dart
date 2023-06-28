c58899035df4b12a0e00ca37ad1bb29048548884
Steffan Mouton <moutonsteffan@gmail.com>
2023-06-27 20:44:05 -0500

    Tab Nav added, along with Favorites Page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 import 'package:meals/screens/categories.dart';
+import 'package:meals/screens/tabs.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -22,8 +23,8 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: theme,
-        home: const CategoriesScreen(),
+      theme: theme,
+      home: const TabsScreen(),
     );
   }
 }
--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -8,16 +8,22 @@ import '../data/dummy_data.dart';
 import '../models/meal.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key, required this.title, required this.meals});
+  const MealsScreen(
+      {super.key,
+      this.title,
+      required this.meals,
+      required this.onToggleFavorite});
 
-  final String title;
+  final String? title;
   final List<Meal> meals;
+  final void Function(Meal meal) onToggleFavorite;
 
   void _selectMeal(BuildContext context, Meal meal) {
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (ctx) => MealDetailsScreen(
           meal: meal,
+          onToggleFavorite: onToggleFavorite,
         ),
       ),
     );
@@ -25,20 +31,26 @@ class MealsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var bodyContent = meals.isEmpty
+        ? const Text('No Meals Available for this Category. Sorry!')
+        : ListView.builder(
+            itemCount: meals.length,
+            itemBuilder: (ctx, index) => MealItem(
+                meal: meals[index],
+                onSelectMeal: () {
+                  _selectMeal(context, meals[index]);
+                }),
+          );
+
+    if (title == null) {
+      return bodyContent;
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(title!),
       ),
-      body: meals.isEmpty
-          ? const Text('No Meals Available for this Category. Sorry!')
-          : ListView.builder(
-              itemCount: meals.length,
-              itemBuilder: (ctx, index) => MealItem(
-                  meal: meals[index],
-                  onSelectMeal: () {
-                    _selectMeal(context, meals[index]);
-                  }),
-            ),
+      body: bodyContent,
     );
   }
 }
