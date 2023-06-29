1066dcdf84c79cadd6f0228bbb2573a9c70d38d1
Steffan Mouton <moutonsteffan@gmail.com>
2023-06-29 14:26:05 -0500

    Added filters and logic for filtering meals

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -7,22 +7,25 @@ import 'package:meals/widgets/category_grid_item.dart';
 import '../models/meal.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key, required this.onToggleFavorite});
+  const CategoriesScreen(
+      {super.key, required this.onToggleFavorite, required this.availableMeals});
 
+  final List<Meal> availableMeals;
   final void Function(Meal meal) onToggleFavorite;
 
   void _selectCategory(BuildContext context, Category category) {
-    final filteredMeals = dummyMeals
+    final filteredMeals = availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
 
     Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (ctx) => MealsScreen(
-          title: category.title,
-          meals: filteredMeals,
-          onToggleFavorite: onToggleFavorite,
-        ),
+        builder: (ctx) =>
+            MealsScreen(
+              title: category.title,
+              meals: filteredMeals,
+              onToggleFavorite: onToggleFavorite,
+            ),
       ),
     );
   }
